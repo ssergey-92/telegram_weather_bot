bf1c83c4d78a469bf8ed8235ec03a50e0d68852a
Sergey Solop <solop1992@mail.ru>
2024-10-14 23:12:53 +0300

    feat: add comments in .env.template

--- .env.template.py
+++ .env.template.py
@@ -1,16 +1,18 @@
 BOT_TOKEN= # Telegram bot token
 OPEN_WEATHER_MAP_KEY= # Secret Key for OpenWeatherMap
 
-BOT_NAME=
-ENCODING=
+BOT_NAME= # Telegram bot name
+ENCODING= # project encoding
 
-LOGGER_NAME=
-LOGGER_LEVEL=
-LOGGER_FILES_DIR_NAME=
-LOGGER_FILES_NAME=
-LOGGER_STREAM_HANDLER_LEVEL=
-LOGGER_FILE_HANDLER_LEVEL=
-LOGGER_FILE_SIZE=
-LOGGER_BACKUP_COUNT=
+LOGGER_NAME= # Project logger name
+LOGGER_LEVEL= # Project logger level
+LOGGER_FILES_DIR_NAME= # Project logger log files directory name
+LOGGER_FILES_NAME= # Project logger log file name
+LOGGER_STREAM_HANDLER_LEVEL= # Project logger stream handler level
+LOGGER_FILE_HANDLER_LEVEL= # Project logger file handler level
+LOGGER_FILE_SIZE= # Project logger file size
+LOGGER_BACKUP_COUNT= # Project logger files backup count
 
-RUN_IN_DOCKER=
+RUN_IN_DOCKER= # Start Docker with or without a container (True/False)
+DOCKER_LOGGER_MAX_FILES= # Docker logger max backup files
+DOCKER_LOGGER_FILE_SIZE= # Docker logger max file size
